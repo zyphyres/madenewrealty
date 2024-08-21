8e89b02529bf86d4d7bd2418085dfdeab4a6e97e
zyphyres <72875954+zyphyres@users.noreply.github.com>
2024-08-21 12:51:46 +0800

    up

--- homeowner-account-creation.php
+++ homeowner-account-creation.php
@@ -140,19 +140,6 @@ if (isset($_GET['token'])) {
                                         </div>
 
 
-                                        <div class="form-group" id="tosell" style="display: none;">
-                                            <label>Type of Ownership</label>
-                                            <div class="form-check">
-                                                <label class="form-check-label">
-                                                    <input type="radio" class="form-check-input" name="uown" value="Other"> Owned
-                                                    <input type="radio" class="form-check-input" name="uown" value="Other"> Parents
-                                                    <input type="radio" class="form-check-input" name="uown" value="Other"> Relatives
-                                                    <input type="radio" class="form-check-input" name="uown" value="Other"> Friends
-                                                </label>
-                                            </div>
-                                        </div>
-
-
 
 
                                         <div class="form-group">
